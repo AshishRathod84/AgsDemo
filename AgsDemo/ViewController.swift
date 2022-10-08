05ba585bbe0bd34c8ab16a453e8a3d2a602650ae
Ashish Rathod <ashish.rathod@auxanoglobalservices.com>
2022-10-08 16:19:57 +0530

    Initial Commit

---
+++ AgsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AgsDemo
+//
+//  Created by Auxano on 08/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
